a8e690cf3d753f30e68dbfd47192d0a1e3233a22
Chad Dreher <chad@grizzisland.com>
2025-03-12 13:29:14 -0700

    Create fix for script also allow people to set desired length of password

--- pswdgen.py
+++ pswdgen.py
@@ -6,3 +6,13 @@ def generate_password(length=12):
     chars = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(chars) for _ in range(length))
     print("Generated Password:", password)
+
+# Ask the user for the desired password length
+try:
+    length = int(input("Enter the desired password length: "))
+    if length <= 0:
+        print("Password length must be a positive number.")
+    else:
+        generate_password(length)
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
