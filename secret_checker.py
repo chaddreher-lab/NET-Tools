2d6e44731f12ddd159025ce136c4b234c4105ade
Chad Dreher <chad@grizzisland.com>
2025-05-01 15:40:21 -0700

    Update to actually scrape the .env file

--- secret_checker.py
+++ secret_checker.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 import os
 import sys
+
 sys.stdout.reconfigure(encoding='utf-8')
+load_dotenv()  # Load environment variables from .env file
 
 def check_secret(secret_name):
     secret_value = os.getenv(secret_name)
@@ -14,8 +17,13 @@ def main():
     
     # List of secrets to check
     secrets_to_check = [
-        "PASTEBIN_API",  # Add more secrets as needed
+        "PASTEBIN_API",
         "TEST",
+        "UNIFI_HOST",
+        "UNIFI_USERNAME",
+        "UNIFI_PASSWORD",
+        "UNIFI_SITE_ID",
+        "DISCORD_WEBHOOK_URL_UNIFI_PROTECT",
     ]
     
     for secret in secrets_to_check:
